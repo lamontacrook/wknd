eb3decf5e61ddcf9aa29026491752e9c58e528a6
Lamont Crook <lamont@adobe.com>
2023-02-13 14:45:24 -0700

    update

--- blocks/slider-list/slider-list.js
+++ blocks/slider-list/slider-list.js
@@ -1,19 +1,15 @@
 import { createTag } from '../../scripts/scripts.js';
 
 export default async function decorate(block) {
-  [...block.children].forEach(element => {
-    let n = 1;
-    element.querySelectorAll('div>div>p').forEach(item => {
-      const sliderItem = createTag('div', {style: `background-color:${item.innerHTML}`});
-      let h6 = element.querySelector('div>div>h6:nth-child(n)');
-      let h5 = element.querySelector('div>div>h5:nth-child(n)');
+  [...block.children].forEach((element) => {
+    element.querySelectorAll('div>div>p').forEach((item) => {
+      const sliderItem = createTag('div', { style: `background-color:${item.innerHTML}` });
+      const h6 = element.querySelector('div>div>h6');
+      const h5 = element.querySelector('div>div>h5');
       sliderItem.appendChild(h6);
       sliderItem.appendChild(h5);
-      n+=1;
-      console.log(sliderItem);
       element.appendChild(sliderItem);
     });
     element.removeChild(element.querySelector('div>div'));
-    console.log(element);
-  })
+  });
 }
